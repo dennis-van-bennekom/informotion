052e5d3daca8187ee316ad84fda0d657f82ee544
Zevahn <sven.ten.voorde@gmail.com>
2015-10-29 14:12:27 +0100

    styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,29 @@
 $('document').ready(function () {
-	$('.menu-button').click(function () {
-		if ($('.body').hasClass('menu-open')) {
-			$('.body').removeClass('menu-open');
+	var options = {
+    prefetch: true,
+    cacheLength: 2,
+    onStart: {
+      duration: 1000, // Duration of our animation
+      render: function ($container) {
+        // Add your CSS animation reversing class
+        $container.addClass('is-exiting');
 
-		} else {
-			$('.body').addClass('menu-open');
-		}
-	});
+        // Restart your animation
+        smoothState.restartCSSAnimations();
+      }
+    },
+    onReady: {
+      duration: 0,
+      render: function ($container, $newContent) {
+        // Remove your CSS animation reversing class
+        $container.removeClass('is-exiting');
 
-	$('#main-wrapper').smoothState({ 
-		debug: true
-	});
+        // Inject the new content
+        $container.html($newContent);
+
+      }
+    }
+  },
+  smoothState = $('#main-wrapper').smoothState(options).data('smoothState');
 
 });
